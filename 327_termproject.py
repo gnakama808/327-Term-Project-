daf5664a0a09c5c1ce0cef0849d2a96b625a4882
gnakama808 <77822737+gnakama808@users.noreply.github.com>
2021-03-17 01:34:29 -1000

    Add files via upload
    Notes on brainstorming

---
+++ 327_termproject.py
@@ -0,0 +1,17 @@
+print ("Running mock trial")
+
+#TEST AREA/Brainstorm Area
+#Objective: write a program that allows two or more computers to synchronize files via LAN
+#           should also be able to synchronize multidirectionally (both ways, all ways)
+#           Ex. Node P1 has client C1, F1 is the set of all files on P1, etc. etc.
+#           Most likely need "distributed hash table" or DHT
+
+#How does the client discover other clients on the network?
+#How does the client deal with files of the same name but different contents? Different timestamps?
+#How does the client determine the order of syncing with regards to the files of other clients?
+
+#RECALL 326 notes
+#Suggest looking into coding in the Terminal, remember 326 and coding in C and C++
+#Start off small
+#Make one node, connect the node to a client, then make another node and connect the two nodes
+
